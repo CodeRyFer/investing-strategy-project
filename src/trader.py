23b19834d78641e426849e9392448faca115e7eb
Ryan Fehr <coderry4@gmail.com>
2025-09-01 21:31:39 -0500

    Remove unused code

--- src/trader.py
+++ src/trader.py
@@ -6,18 +6,9 @@ import yfinance as yf
 
 style.use('ggplot')
 
-##start = dt.datetime(2000,1,1)
-##end= dt.datetime(2025,8,28)
-##
-##
-##tsla = yf.Ticker('TSLA')
-##df = tsla.history(start=start, end=end)
-##
-##df.to_csv('tsla.csv')
-
 df = pd.read_csv('tsla.csv', parse_dates=True, index_col = 0)
 
-##print(df.head())
+
 
 df['Close'].plot()
 plt.show()
